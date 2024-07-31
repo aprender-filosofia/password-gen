9795f090363da7267db183152b6cff184dc6ae29
Vinicius Barbosa de Aguiar <filosofia.alexandria@gmail.com>
2024-07-31 18:48:12 -0300

    Outline version

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PasswordGen
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Options op = new MainClass.Options();
+            Console.Write("How long is the password: ");
+
+            op.Long = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Capital Letters: [Y/N]: ");
+
+            op.CapitalLetters = Console.ReadLine().ToUpper() == "Y";
+
+            Console.Write("Small Letters? [Y/N]: ");
+
+            op.SmallLetters = Console.ReadLine().ToUpper() == "Y";
+
+            Console.Write("Numbers? [Y/N]: ");
+            op.Numbers = Console.ReadLine().ToUpper() == "Y";
+
+            Console.Write("Special Characters? [Y/N]: ");
+            op.SpecialCharacters = Console.ReadLine().ToUpper() == "Y";
+        }
+
+        class PasswordGeneretor
+        {
+            PasswordGeneretor(Options op)
+            {
+
+            }
+        }
+
+        class Options
+        {
+            public Int32 Long { set; get; }
+            public bool CapitalLetters { set; get; }
+            public bool SmallLetters { set; get; }
+            public bool Numbers { set; get; }
+            public bool SpecialCharacters { set; get; }
+        }
+    }
+}
